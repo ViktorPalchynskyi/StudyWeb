d5fca6fabc0e188cac2b44a7b0aa1defa1c7b326
Arlecuno <palchinskij19@gmail.com>
2021-07-21 08:53:00 +0300

    study ts intersection types

--- ts/course_1/lesson_1/Index.ts
+++ ts/course_1/lesson_1/Index.ts
@@ -1,30 +1,3 @@
-interface CoordinateInterface {
-   x: number;
-   y: number;
-}
-
-type CoordinateTurple = [number, number];
-type Coordinate = CoordinateInterface | CoordinateTurple;
-
-const extractCoordinate = (poin: Coordinate): CoordinateInterface => {
-   if (Array.isArray(poin)) {
-      return { x: poin[0], y: poin[1] };
-   }
-   return poin;
-};
-
-const getDistance = (point1: Coordinate, point2: Coordinate): number => {
-   const { x: x1, y: y1 } = extractCoordinate(point1);
-   const { x: x2, y: y2 } = extractCoordinate(point2);
-
-   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-}
-
-const point1: CoordinateInterface = { x: 10, y: 14 };
-const point2: CoordinateTurple = [16, 22];
-
-console.log('Distance is', getDistance(point1, point2));
-
 interface Fruit {
    name: string;
    sweetness: number;
@@ -35,14 +8,6 @@ interface Vegetable {
    hasSeed: boolean;
 }
 
-type union = Fruit | Vegetable;
+type EdibleThing = Fruit & Vegetable;
 
-const hasSeed = (some: union): void => {
-   if ('hasSeed' in some) {
-      console.log(some.hasSeed);
-   }
-};
-
-const tmp: union = { name: 'Some', hasSeed: true, sweetness: 290 };
-
-hasSeed(tmp);
+const apple: EdibleThing = { name: 'apple', sweetness: 44, hasSeed: true };
