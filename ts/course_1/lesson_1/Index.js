fee04fd85166c4052f3f53b9f0ba82f93c076914
Arlecuno <palchinskij19@gmail.com>
2021-07-27 22:27:35 +0300

    study ts working with ts classes

--- ts/course_1/lesson_1/Index.js
+++ ts/course_1/lesson_1/Index.js
@@ -1,21 +1,70 @@
-var extractCoordinate = function (poin) {
-    if (Array.isArray(poin)) {
-        return { x: poin[0], y: poin[1] };
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var EdibleThing = /** @class */ (function () {
+    function EdibleThing(name, color) {
+        this.name = name;
+        this.color = color;
     }
-    return poin;
-};
-var getDistance = function (point1, point2) {
-    var _a = extractCoordinate(point1), x1 = _a.x, y1 = _a.y;
-    var _b = extractCoordinate(point2), x2 = _b.x, y2 = _b.y;
-    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-};
-var point1 = { x: 10, y: 14 };
-var point2 = [16, 22];
-console.log('Distance is', getDistance(point1, point2));
-var hasSeed = function (some) {
-    if ('hasSeed' in some) {
-        console.log(some.hasSeed);
+    return EdibleThing;
+}());
+var Fruit = /** @class */ (function (_super) {
+    __extends(Fruit, _super);
+    function Fruit(name, color, sweetness) {
+        var _this = _super.call(this, name, color) || this;
+        _this.sweetness = sweetness;
+        return _this;
     }
-};
-var tmp = { name: 'Some', hasSeed: true, sweetness: 290 };
-hasSeed(tmp);
+    Fruit.prototype.fullName = function () {
+        var isSweet = this.sweetness > 50;
+        return (isSweet ? 'Sweet' : '') + " " + this.color + " " + this.name;
+    };
+    ;
+    return Fruit;
+}(EdibleThing));
+var apple = new Fruit('Apple', 'Red', 55);
+console.log(apple.fullName());
+var fruitBasket = [];
+fruitBasket.push(new Fruit('Banana', 'Yellow', 103));
+console.log(fruitBasket);
+var Vegetable = /** @class */ (function () {
+    function Vegetable(name) {
+        this.name = name;
+    }
+    Vegetable.cookVegetable = function (vegetable) {
+        setTimeout(function () { return console.log("Cooked " + vegetable.name); }, Vegetable.timeToCook * 1000);
+    };
+    Vegetable.timeToCook = 5;
+    return Vegetable;
+}());
+Vegetable.cookVegetable(new Vegetable('Tomato'));
+var Some = /** @class */ (function () {
+    function Some(name) {
+        this.name = name;
+    }
+    return Some;
+}());
+var Fruit2 = /** @class */ (function (_super) {
+    __extends(Fruit2, _super);
+    function Fruit2(name) {
+        return _super.call(this, name) || this;
+    }
+    Fruit2.prototype.doSomething = function () {
+        console.log("Yum. " + this.name + "s are tasty.");
+    };
+    return Fruit2;
+}(Some));
+var fruit = new Fruit2('Mellon');
+fruit.doSomething();
--- ts/course_1/lesson_1/Index.ts
+++ ts/course_1/lesson_1/Index.ts
@@ -1,13 +1,66 @@
-interface Fruit {
+class EdibleThing {
    name: string;
+   color: string;
+   constructor(name: string, color: string) {
+      this.name = name;
+      this.color = color;
+   }
+}
+
+class Fruit extends EdibleThing {
    sweetness: number;
+
+   constructor(name: string, color: string, sweetness: number) {
+      super(name, color);
+      this.sweetness = sweetness;
+   }
+
+   fullName(): string {
+      const isSweet: boolean = this.sweetness > 50;
+      return `${isSweet ? 'Sweet' : ''} ${this.color} ${this.name}`;
+   };
 }
 
-interface Vegetable {
+const apple = new Fruit('Apple', 'Red', 55);
+
+console.log(apple.fullName());
+
+const fruitBasket: Fruit[] = [];
+
+fruitBasket.push(new Fruit('Banana', 'Yellow', 103));
+console.log(fruitBasket);
+
+class Vegetable {
+   static timeToCook: number = 5;
+   static cookVegetable(vegetable: Vegetable): void {
+      setTimeout(() => console.log(`Cooked ${vegetable.name}`), Vegetable.timeToCook * 1000);
+   }
    name: string;
-   hasSeed: boolean;
+   constructor(name: string) {
+      this.name = name;
+   }
 }
 
-type EdibleThing = Fruit & Vegetable;
+Vegetable.cookVegetable(new Vegetable('Tomato'));
 
-const apple: EdibleThing = { name: 'apple', sweetness: 44, hasSeed: true };
+abstract class Some {
+   name: string;
+   abstract doSomething(): void;
+
+   constructor(name: string) {
+      this.name = name;
+   }
+}
+
+class Fruit2 extends Some {
+   constructor(name: string) {
+      super(name);
+   }
+   doSomething() {
+      console.log(`Yum. ${this.name}s are tasty.`);
+   }
+}
+
+const fruit: Fruit2 = new Fruit2('Mellon');
+fruit.doSomething();
+
