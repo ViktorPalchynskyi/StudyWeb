d37ff5a80f5d8b5ad87ad1e42f7b3b229fcec7d4
Viktor Palchynskyi <palchinskij19@gmail.com>
2022-05-22 19:45:34 +0300

    repeated event listener

--- js/modern_java_bootcamp/app.js
+++ js/modern_java_bootcamp/app.js
@@ -1,118 +1,36 @@
-const warriorsGames = [{
-   awayTeam: {
-     team: 'Golden State',
-     points: 119,
-     isWinner: true
-   },
-   homeTeam: {
-     team: 'Houston',
-     points: 106,
-     isWinner: false
-   }
- },
- {
-   awayTeam: {
-     team: 'Golden State',
-     points: 105,
-     isWinner: false
-   },
-   homeTeam: {
-     team: 'Houston',
-     points: 127,
-     isWinner: true
-   }
- },
- {
-   homeTeam: {
-     team: 'Golden State',
-     points: 126,
-     isWinner: true
-   },
-   awayTeam: {
-     team: 'Houston',
-     points: 85,
-     isWinner: false
-   }
- },
- {
-   homeTeam: {
-     team: 'Golden State',
-     points: 92,
-     isWinner: false
-   },
-   awayTeam: {
-     team: 'Houston',
-     points: 95,
-     isWinner: true
-   }
- },
- {
-   awayTeam: {
-     team: 'Golden State',
-     points: 94,
-     isWinner: false
-   },
-   homeTeam: {
-     team: 'Houston',
-     points: 98,
-     isWinner: true
-   }
- },
- {
-   homeTeam: {
-     team: 'Golden State',
-     points: 115,
-     isWinner: true
-   },
-   awayTeam: {
-     team: 'Houston',
-     points: 86,
-     isWinner: false
-   }
- },
- {
-   awayTeam: {
-     team: 'Golden State',
-     points: 101,
-     isWinner: true
-   },
-   homeTeam: {
-     team: 'Houston',
-     points: 92,
-     isWinner: false
-   }
- }
-]
+const el = document.querySelector('h1');
+const btn = document.querySelector('button');
 
-const makeChart = (games, targetTeam) => {
-   const ulParent = document.createElement('ul');
-   const title = document.createElement('h1');
-   title.innerText = targetTeam;
-   ulParent.prepend(title);
-   for (const game of games) {
-      const gameLi = document.createElement('li'); 
-      gameLi.innerHTML = getScoreLine(game);
-      gameLi.classList.add(checkWinner(game, targetTeam) ? 'win' : 'loss');
-      ulParent.appendChild(gameLi);
-   }
-   return ulParent;
-};
+el.addEventListener('mouseover', function(){this.style.color = 'coral'});
+el.addEventListener('mouseout', function(){this.style.color = 'black'});
 
-const getScoreLine = ({ awayTeam, homeTeam }) => {
-   const { team: hTeam , points: hPoints } = homeTeam;
-   const { team: aTeam , points: aPoints } = awayTeam;
-   const teamNames = `${ aTeam } @ ${ hTeam }`;
-   let scoreLine;
-   aPoints > hPoints ? 
-         scoreLine = `<b>${ aPoints }</b>-${ hPoints }`: 
-         scoreLine = `${ aPoints }-<b>${ hPoints }</b>`;
-   return `${ teamNames } ${scoreLine }`;
-};
+btn.addEventListener('mouseover', function() {
+   const x = Math.floor(Math.random() * (window.innerWidth));
+   const y = Math.floor(Math.random() * (window.innerHeight));
+   this.style.transform = `translate(${x}px, ${y}px)`;
+});
 
-const checkWinner = ({ awayTeam, homeTeam }, targetTeam) => {
-   const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
-   return target.isWinner;
-}
 
-document.body.appendChild(makeChart(warriorsGames, 'Houston'));
-document.body.appendChild(makeChart(warriorsGames, 'Golden State'));
+btn.addEventListener('click', function() {
+   console.log('action');
+   document.body.style.backgroundColor = 'green';
+});
+
+document.body.addEventListener('keypress', (event) => {
+   if (event.key === 'c') return console.log('Ahhhh Shit. Here we go again.') 
+});
+
+document.body.addEventListener('keydown', (event) => {
+   if (event.key === 'j') return console.log('Ahhhh Shit. Fuck!.') 
+});
+
+document.body.addEventListener('keydown', (event) => {
+   if (event.key === 'j') return console.log('Ahhhh Shiiiiit') 
+});
+
+const form = document.querySelector('#signup-form');
+form.addEventListener('submit', function(event) {
+   event.preventDefault();
+   alert('Rem');
+   console.dir(this);
+})
