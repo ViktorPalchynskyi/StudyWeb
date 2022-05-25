ff846131e6e45dd7fedffe2491322016c13fec6a
Viktor Palchynskyi <palchinskij19@gmail.com>
2022-05-25 10:42:54 +0300

    working with api calls

--- js/modern_java_bootcamp/app.js
+++ js/modern_java_bootcamp/app.js
@@ -1,31 +1,56 @@
-const btn = document.querySelector('button');
+// const req = new XMLHttpRequest();
+// req.addEventListener('load', function() {
+//    console.log('IT WOKED!!');
+//    const result = JSON.parse(this.responseText);
+//    const filmURL = result.results[0].films[0];   
+//    const filmReq = new XMLHttpRequest();
 
-// setTimeout(() => {
-//    btn.style.transform = `translateX(100px)`
-//    setTimeout(() => {
-//       btn.style.transform = `translateX(200px)`
-//    }, 1 * 1000);
-// }, 1 * 1000);
+//    filmReq.addEventListener('load', function() {
+//       console.log('Film data', JSON.parse(this.responseText));
+//    });
+   
+//    filmReq.addEventListener('error', () => {
+//       console.log('filmReq ERROR');
+//    });
+   
+//    filmReq.open('GET', filmURL);
+//    filmReq.send();
+// });
 
-const moveX = (element, amount, delay) => {
-   return new Promise((resolve, reject) => {
-      setTimeout(() => {
-         const bodyBoundary = document.body.clientWidth;
-         const currentRight = btn.getBoundingClientRect().right;
-         const currentLeft = btn.getBoundingClientRect().left;
-         if(currentRight + amount > bodyBoundary) {
-            reject('GG WP');
-         } else {
-            element.style.transform = `translateX(${currentLeft + amount}px)`;
-            resolve();
-         }
-      }, delay);
-   });   
-};
+// req.addEventListener('error', () => {
+//    console.log('ERROR');
+// });
 
-moveX(btn, 500, 1000)
-   .then(() => {
-      return moveX(btn, 500, 1000);
-   })
-   .then(() => console.log('second'))
-   .catch(() => alert('GG WP'));
+// req.open('GET', 'https://swapi.dev/api/planets/');
+// req.send();
+// console.log('REQUEST SEND');
+
+// const req = fetch('https://swapi.dev/api/planets/');
+
+// const checkStatusAndParse = (res) => {
+//    console.log(res);
+//    if (!res.ok) throw new Error(`Status Code Error: ${res.status}`);
+//    else return res.json();
+// }
+
+// req
+// .then(checkStatusAndParse)
+// .then(data => {
+//    console.log('data', data);
+//    const planetURL = data.results[3].films[0];
+//    console.log(data);
+//    return fetch(planetURL);
+// })
+// .then(checkStatusAndParse)
+// .then(data => {
+//    console.log('data2', data);
+//    const h1 = document.createElement('h1');
+//    const p = document.createElement('p');
+//    h1.innerText = data.title;
+//    p.innerText = data.opening_crawl;
+//    document.body.append(h1,p)
+// })
+// .catch(err => console.log(err));
+
+axios.get('https://swapi.dev/api/planets/')
+   .then(res => console.log(res))
