2c064a15e3555dc0682912e3e34d98c1254db5b3
Viktor Palchynskyi <palchinskij19@gmail.com>
2022-05-30 13:45:48 +0300

    secret message project practice

---
+++ js/modern_java_bootcamp/part2/secret-message/index.js
@@ -0,0 +1,21 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+   document.querySelector('#message-form').classList.toggle('hide');
+   document.querySelector('#message-show').classList.toggle('hide');
+
+   document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+   event.preventDefault();
+   document.querySelector('#message-form').classList.toggle('hide');
+   document.querySelector('#link-form').classList.toggle('hide');
+   const input = document.querySelector('#message-input');
+   const encrypted = btoa(input.value);
+   const linkInput = document.querySelector("#link-input");
+   linkInput.value = `${window.location}#${encrypted}`;
+   linkInput.select();
+});
