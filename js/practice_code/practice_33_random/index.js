253323601fc42d48fef0e0c203b0f7f959cc42d6
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-02-07 10:20:06 +0300

    working with es5 constructor functions

--- js/practice_code/practice_33_random/index.js
+++ js/practice_code/practice_33_random/index.js
@@ -1,48 +1,54 @@
-const user = Object.defineProperties({}, {
-   name: {
-      value: 'misha',
-      writable: true,
-      configurable: true,
-      enumerable: true,
-   },
-   age: {
-      value: 25,
-      writable: true,
-      configurable: true,
-      enumerable: true,
-   }
+function Person(first, last, age, gender, interests = []) {
+   this.name = { 
+      first,
+      last,
+   };
+   this.age = age;
+   this.gender = gender;
+   this.interests = interests;
+};
+
+Person.prototype.greeting = function() {
+   console.log(`Hi I'm ${this.name.first}`);
+};
+
+Person.prototype.countInterests = function () {
+   return this.interests.length;
+};
+
+function Teacher(first, last, age, gender, interests, subject) {
+   Person.call(this, first, last, age, gender, interests,);
+
+   this.subject = subject;
+}
+
+Teacher.prototype = Object.create(Person.prototype);
+Object.defineProperty(Teacher.prototype, 'constructor', {
+   value: Teacher,
+   enumerable: false,
+   writable: true,
 });
 
-function processNums(multiplayer) {
-   return (this.a + this.b) * multiplayer;
-}
-
-const objProt = { name: 'hello' };
-const obj = { a: 22, b: 10 };
-obj.__proto__ = objProt;
-
-function Person(name) {
-   this.name = name;
+Teacher.prototype.greeting = function() {
+   let prefix;
+ 
+   if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
+     prefix = 'Mr.';
+   } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
+     prefix = 'Mrs.';
+   } 
+ 
+   console.log(`Hello. My name is ${prefix} ${this.name.last} and I teach ${this.subject}`);
 };
 
-function Employee(name, salary) {
-   this.name = name;
-   this.salary = salary;
-}
-
-Person.prototype = {
-   hands: 2, 
-   legs: 2,
-   sleep: function () {
-      console.log(`${this.name} is sleeping`);
-   }
+Teacher.prototype.countInterests = function () {
+   let defauldCount = Person.prototype.countInterests.call(this);
+   return ++defauldCount;
 };
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+const sensei = new Teacher('Satoshi', 'Tanaka', 22, 'm', ['anime', 'manga', 'sudoku'], 'Art');
 
-const persOne = new Person('Viktor');
-const rabotyaga = new Employee('Masha', 200);
+console.log('sensei', sensei);
+sensei.greeting();
 
-console.log('rabotyaga', rabotyaga);
-rabotyaga.sleep();
+console.log('new counter', sensei.countInterests());
