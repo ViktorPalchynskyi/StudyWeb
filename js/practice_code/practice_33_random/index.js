a4d42c9996ef8032d9b6aa6701bb656dce9860fa
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-02-07 11:28:49 +0300

    working with es6 classes

--- js/practice_code/practice_33_random/index.js
+++ js/practice_code/practice_33_random/index.js
@@ -48,7 +48,56 @@ Teacher.prototype.countInterests = function () {
 
 const sensei = new Teacher('Satoshi', 'Tanaka', 22, 'm', ['anime', 'manga', 'sudoku'], 'Art');
 
-console.log('sensei', sensei);
-sensei.greeting();
+class User {
+   constructor(name, age, status) {
+      this.name = name;
+      this.age = age;
+      this.status = status;
+      this.messageCount = 0;
+   }
 
-console.log('new counter', sensei.countInterests());
+   get name() {
+      return this._name;
+   }
+
+   set name(val) {
+      if (val.length < 3) {
+         console.log('Name is too short');
+         return;
+      }
+      this._name = val;
+   }
+
+   writeMessage(text) {
+      console.log(`User ${this.name} have just wrotten a message with text: ${text}`);
+      this.messageCount++;
+   }
+
+   countMessages() {
+      console.log(`User: ${this.name} have send ${this.messageCount} messages`);
+      return this.messageCount;
+   }
+}
+
+class Admin extends User {
+   constructor(name, age, status, adminLevel) {
+      super(name, age, status);
+      this.adminLevel = adminLevel;
+   }
+   checkData() {
+      return console.log(`Admin ${this.name} checked data`);
+   }
+
+   countMessages() {
+      let sendedMessages = super.countMessages();
+      sendedMessages++;
+      return sendedMessages;
+   }
+}
+
+const admin1 = new Admin('Lena', 25, 'online', 1);
+
+admin1.writeMessage('Hello there');
+admin1.checkData();
+console.log('lala', admin1.countMessages());
+console.log(admin1.adminLevel)
