ba9a21a1e9619268a2dcce8cc9e67de61f373c35
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-02-14 13:53:47 +0300

    clean up

--- js/react/repetition/src/App.js
+++ js/react/repetition/src/App.js
@@ -1,10 +1,5 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import {  Link } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import Book from './pages/books/Book';
-import NewBook from './pages/books/NewBook';
-import NotFound from './pages/NotFound';
-import BooksLayout from './pages/books/BooksLayout';
 import AppRoutes from './router';
 
 function App() {
