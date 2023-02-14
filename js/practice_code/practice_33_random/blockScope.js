64cba8e5d906eb54f5c75552022babc3988c6d4c
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-02-14 14:34:44 +0300

    block scope js

---
+++ js/practice_code/practice_33_random/blockScope.js
@@ -0,0 +1,35 @@
+for (var i = 0; i < 10; i++) {
+   // console.log('i', i);
+}
+
+for (var i = 0; i < 15; i++) {
+   // console.log('i', i);
+}
+
+// console.log('i after loop', i);
+
+if(i > 0) {
+   {
+      var b = 123;
+      let c = 223;
+   }
+   // console.log('c', c);
+}
+
+// b = 9;
+// console.log('b', b);
+// console.log('c', c);
+var z = 23;
+var z = 444;
+// console.log('z', z);
+
+var j = 222;
+
+function some(num) {
+   var j = 12;
+   return num * j;
+}
+
+j = 10;
+
+console.log('some', some(10));
