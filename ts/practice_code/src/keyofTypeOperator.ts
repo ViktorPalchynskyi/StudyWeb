e3c1c56d5719870870ee56b03966fd4f2c0d903f
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-02-21 10:25:30 +0300

    keysOfOperator

---
+++ ts/practice_code/src/keyofTypeOperator.ts
@@ -0,0 +1,11 @@
+type Table = {
+   width: number;
+   height: number;
+   mark: string;
+};
+
+type TKeys = keyof Table;
+
+type Arrayish = { [n: string]: unknown };
+
+type AKeys = keyof Arrayish;
