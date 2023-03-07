b478b7a32540f0d3c404575bd4f559c3cd9d6d0e
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-03-07 20:51:38 +0300

    setTimeout setInterval

---
+++ js/practice_code/practice_33_random/timing.js
@@ -0,0 +1,8 @@
+// let timerId = setTimeout(function tick() {
+//    console.log('tick');
+//    timerId = setTimeout(tick, 2000);
+// }, 2000);
+
+setTimeout(() => console.log('Briki'));
+
+console.log('Chiki');
---
+++ js/practice_code/practice_35_learn_js_course/leson2.js
@@ -0,0 +1,16 @@
+// function sortString(arr = [], param = 'asc') { 
+//    const directions = {
+//       asc: 1,
+//       desc: -1
+//    };
+
+//    const direction = directions[param];
+
+//    if (typeof direction === undefined) {
+//       throw new Error('Some')
+//    }
+
+//    return [...arr].sort((string1, string2) => {
+//       return direction * string1.
+//    });
+//  }
