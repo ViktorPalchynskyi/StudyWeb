cbf51a161b5795427b23f748aea90bf8bb171fb8
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-03-13 13:21:55 +0300

    conditional type extention

--- ts/practice_code/src/conditionalTypes.ts
+++ ts/practice_code/src/conditionalTypes.ts
@@ -18,4 +18,29 @@ const b = createLabel('123');
 type FunctionReturn<FunctionType extends (...args: any[]) => any> =
    FunctionType extends (...args: any[]) => infer ReturnType ? ReturnType : never;
 
-type ReturnType = FunctionReturn<typeof createLabel>
+type ReturnType = FunctionReturn<typeof createLabel>;
+
+type MessageOf<Type> = Type extends { message: unknown } ? Type['message'] : never;
+
+interface Email {
+   message: { text: string, id: string };
+}
+
+interface Kush {
+   kurlyk: () => void;
+   gavGav(): void;
+}
+
+type EmailMessageContentType = MessageOf<Email>;
+type KushMessageContentType = MessageOf<Kush>;
+
+type Flatten<Type> = Type extends Array<infer Item> ? Item : Type;
+
+type Tmp = Flatten<Array<Email>>;
+
+type ToArray<Type> = Type extends any ? Type[] : never;
+type ToArray2<Type> = [Type] extends [any] ? Type[] : never;
+
+type StrOrNumArr = ToArray<Email | Kush>;
+type StrOrNumArr2 = ToArray2<Email | Kush>;
+
