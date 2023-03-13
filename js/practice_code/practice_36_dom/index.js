ec551521e917de7fba542870215c4e1e6bb7d7c6
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-03-13 12:46:38 +0300

    dom selectors

--- js/practice_code/practice_36_dom/index.js
+++ js/practice_code/practice_36_dom/index.js
@@ -8,32 +8,25 @@ const div2 = document.querySelector('.buya');
 const body = document.body;
 const input1 = document.getElementById('input1');
 const img = document.getElementById('img');
-
-console.log('navigator', navigator.userAgent);
-
 const button = document.createElement('button');
-
 button.innerText = 'Click me';
-
 button.style.backgroundColor = 'violet';
-
 div2.before(button);
-
 button.addEventListener('click', () => {
    location.href = 'https://github.com/ViktorPalchynskyi'
 });
-
 const div3 = document.createElement('div');
-
 div3.innerHTML = `
    <p>Hello there</p>
    <button>Click me too</button>
 `;
-
 div2.insertAdjacentElement('afterbegin', div3);
 
-const clone = div3.cloneNode(true);
 
-setTimeout(()=> div3.remove(), 2000);
+for (const child of body.children) {
+   if (child.matches('.order')) {
+      console.log('child', child);
+   }
+   console.log(child.closest('.buya'));
+}
 
-body.append(clone);
