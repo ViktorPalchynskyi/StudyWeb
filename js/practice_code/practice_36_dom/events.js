813f97374510826cd9fc8e4538d18dbba8caef33
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-03-18 19:15:02 +0300

    scroll practice

--- js/practice_code/practice_36_dom/events.js
+++ js/practice_code/practice_36_dom/events.js
@@ -1,15 +1,20 @@
 const testInput = document.getElementById('keyPractive');
+const testScroll = document.getElementById('scrolPractive');
 let count = 0;
 
 document.addEventListener('keydown', (event) => {
    testInput.value = event.code;
-   count++;
-   console.log('count', count);
    
+   if(event.code === 'ArrowUp' || event.code === 'ArrowDown') event.preventDefault();
+
    if(event.code === 'KeyQ' && event.ctrlKey && event.shiftKey) alert('hotkey');
 
 });
 
 document.addEventListener('keyup', (event) => {
    testInput.value = event.key;
+});
+
+window.addEventListener('scroll', (event) => {
+   testInput.value = pageYOffset + 'px';
 });
